e0d48073cbb326011897f7d60b31dcfec3f339d2
susmitha2483 <115517215+susmitha2483@users.noreply.github.com>
2023-12-31 10:38:16 +0530

    Add files via upload

---
+++ 4a.php
@@ -0,0 +1,31 @@
+<?php
+// Assuming you have a MySQL database with a table named 'users'
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "form";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST["username"];
+    $email = $_POST["email"];
+    $password = password_hash($_POST["password"], PASSWORD_DEFAULT);
+
+    $sql = "INSERT INTO `register` (`username`, `email`, `password`) VALUES ('$username', '$email', '$password')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "Registration successful.";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+}
+
+$conn->close();
+?>
